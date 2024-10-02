2efad4c8657d10b832b65cc6526f415858cdbf8d
Daniel <brandanyel@gmail.com>
2024-10-02 20:07:48 +0300

    adding default tables

---
+++ Otto-Database/Tables/ModuleLicenses.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[ModuleLicenses]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [ModuleId] INT NOT NULL, 
+    [UserId] INT NOT NULL
+)
---
+++ Otto-Database/Tables/Users.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NCHAR(10) NULL, 
+    [Email] NCHAR(100) NULL, 
+    [Phone] NCHAR(100) NULL
+)
